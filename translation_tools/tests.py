ea33093eff745e991c6e1e13022631cf259ad79e
Dmitry Dudkevich <dudkevichd@aisec.co.il>
2024-11-30 15:32:34 +0300

    Add base translation class for llama3.1-b and tests

---
+++ translation_tools/tests.py
@@ -0,0 +1,30 @@
+import unittest
+
+from translation_tools.utils import LoadBalancedTranslator
+
+
+class TestLoadBalancedTranslator(unittest.TestCase):
+
+    def setUp(self):
+        self.translator = LoadBalancedTranslator()
+
+    def test_translation(self):
+        texts_to_translate = [
+            "مرحبًا، كيف حالك؟",
+            "كيف هو الطقس اليوم؟",
+            "ما الجديد في العالم؟", 
+            "ما هو فيلمك المفضل؟",
+            "ما هي فوائد استخدام الذكاء الاصطناعي؟",
+        ]
+
+        target_languages = ["en", "de", "es"]
+
+        for text in texts_to_translate:
+            translations = self.translator.get_translations(text, src_lang="ar", tgt_langs=target_languages)
+
+            self.assertTrue(all(lang in translations['translate'] for lang in target_languages))
+            self.assertTrue(text in self.translator.buffer_text)
+            self.assertLessEqual(len(self.translator.buffer_text), 3)
+
+if __name__ == '__main__':
+    unittest.main()
--- translation_tools/utils.py
+++ translation_tools/utils.py
@@ -1,19 +1,47 @@
-from typing import Dict, List
-from translation_tools.cerebras.translator import CerebrasTranslator
-from translation_tools.groq.translator import GroqTranslator
+import json
+from typing import Any, Dict, List
+from translation_tools.llama_translator import BaseTranslator
+from cerebras.cloud.sdk import Cerebras
+from groq import Groq
+
+
+class CerebrasTranslator(BaseTranslator):
+    def __init__(self, client=None, buffer_text=None):
+        super().__init__(client if client else Cerebras(
+            api_key="csk-wdpk43pkx2n439jc9c8pf9wy5vrhtje6c8pyfcyvy9x3jnhc"), buffer_text
+        )
+        self.model = "llama3.1-70b"
+
+
+class GroqTranslator(BaseTranslator):
+    def __init__(self, client=None, buffer_text=None):
+        super().__init__(client if client else Groq(
+            api_key="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"), buffer_text
+        )
+        self.model = "llama3-groq-70b-8192-tool-use-preview"
 
 
 class LoadBalancedTranslator:
-    def __init__(self):
-        self.cerebras_translator = CerebrasTranslator()
-        self.groq_translator = GroqTranslator()
-        self.use_cerebras = True
+    def __init__(self, translators: List[Any] = None):
+        self.counter = 0
+        self.buffer_text = []
+        if translators:
+            self.translators = [translator(
+                buffer_text=self.buffer_text) for translator in translators]
+        else:
+            self.translators = [
+                GroqTranslator(buffer_text=self.buffer_text),
+                CerebrasTranslator(buffer_text=self.buffer_text)
+            ]
 
     def get_translations(self, text: str, src_lang: str = "ar", tgt_langs: List[str] = None) -> Dict[str, str]:
-        if self.use_cerebras:
-            translations = self.cerebras_translator.get_translations(text, src_lang, tgt_langs)
-        else:
-            translations = self.groq_translator.get_translations(text, src_lang, tgt_langs)
+        translator_index = self.counter % len(self.translators)
+        translator = self.translators[translator_index]
+        translations = translator.get_translations(text, src_lang, tgt_langs)
 
-        self.use_cerebras = not self.use_cerebras
+        self.buffer_text.append(text)
+        if len(self.buffer_text) > 3:
+            self.buffer_text.pop(0)
+
+        self.counter += 1
         return translations
